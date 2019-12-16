1928268f702ac245535a6449666aa0663cda2d2a
iustitia <jkubisztal@gmail.com>
2019-12-16 16:30:30 +0100

    class with bookstore

---
+++ class.py
@@ -0,0 +1,24 @@
+class Book:
+
+    def __init__(self, title, author='', isbn='', price=0, count=0):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.price = price
+        self.count = count
+
+    def sell(self):
+        pass
+
+    def restock(self, count):
+        pass
+
+
+class Bookstore:
+
+    def __init__(self):
+        books = []
+
+    def restock_bookstore(self, book, count):
+        pass
+
